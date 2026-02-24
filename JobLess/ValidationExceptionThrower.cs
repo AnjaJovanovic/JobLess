32317c919c032083893f4aeeae3df93cbb4fe696
JelenaMitrovicNT <jelena.mitrovic@naissus.tech>
2026-02-24 11:00:43 +0100

    Inicijalni commit za Oglase

---
+++ JobLess/ValidationExceptionThrower.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JobLess.Shared.Domain.Common
+{
+    public class ValidationExceptionThrower : IValidationExceptionThrower
+    {
+        public void ThrowValidationException(string name, string message)
+        {
+            throw new FluentValidation.ValidationException(
+                new List<ValidationFailure> {
+                new(name,message)
+                }
+            );
+        }
+    }
+
+}
