809e483d36271d92b1483fceb47c54e9b3a1ae54
Luna Rancic <rancicluna00@gmail.com>
2026-06-27 03:45:48 +0200

    IdentityExtensions: dodata konfiguracija MassTransit-a sa RabbitMQ transportom i fanout exchange-om za razmenu poruka

--- JobLess/src/Security/JobLess.IdentityServer.Infrastructure/Extensions/IdentityExtensions.cs
+++ JobLess/src/Security/JobLess.IdentityServer.Infrastructure/Extensions/IdentityExtensions.cs
@@ -4,12 +4,15 @@ using JobLess.IdentityServer.Application.Interfaces;
 using JobLess.IdentityServer.Domain.Entities;
 using JobLess.IdentityServer.Infrastructure.Persistence;
 using JobLess.IdentityServer.Infrastructure.Services;
+using MassTransit;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
+
+using JobLess.Contracts.Events;
 
 namespace JobLess.IdentityServer.Infrastructure.Extensions
 {
@@ -54,6 +57,27 @@ namespace JobLess.IdentityServer.Infrastructure.Extensions
                 ValidAudience = jwtSection["Audience"],
                 IssuerSigningKey = new SymmetricSecurityKey(key)
             };
+        });
+
+        services.AddMassTransit(x =>
+        {
+           x.UsingRabbitMq((ctx, cfg) =>
+           {
+               var rabbitHost = configuration["RabbitMq:Host"] ?? "localhost";
+                    var rabbitUser = configuration["RabbitMq:Username"] ?? "guest";
+                    var rabbitPass = configuration["RabbitMq:Password"] ?? "guest";
+
+                    cfg.Host(rabbitHost, "/", h =>
+                    {
+                        h.Username(rabbitUser);
+                        h.Password(rabbitPass);
+                    });
+
+                    cfg.Message<UserRegisteredMessage>(
+                        m => m.SetEntityName("jobless-user-registered"));
+
+                    cfg.Publish<UserRegisteredMessage>(p => p.ExchangeType = "fanout");
+           }); 
         });
         services.AddScoped<IAuthenticationService, AuthenticationService>();
         services.AddScoped<IJwtTokenService, JwtTokenService>();
